f15c8c412f207fc550012019731eefe20e2f02bd
aask124 <2ara25at1@gmail.com>
2021-12-02 10:38:34 +0900

    削除理由

--- gitp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Hellogitフォルダ直下にこのファイルを設置し、./gitp.sh [変更したいファイル名] [コメント]　で実行可能
-git add $1
-git commit -m "$2"
-git push
--- hello.c
+++
@@ -1,7 +0,0 @@
-#include<stdio>
-//こんにちは!と出力するプログラム
-printf("こんにちは!");
-//英語でhello!と出力するプログラム
-printf("hello!")
-//aaa
-
